fc2ae56c95b0e55a95be1832e669dbb5612271dc
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2020-12-23 21:20:51 -0300

    Remove WSGIServer thread

--- zwift_offline.py
+++ zwift_offline.py
@@ -1706,9 +1706,7 @@ def run_standalone(passed_online, passed_global_pace_partners, passed_global_bot
         return User.query.get(int(uid))
 
     server = WSGIServer(('0.0.0.0', 443), app, certfile='%s/cert-zwift-com.pem' % SSL_DIR, keyfile='%s/key-zwift-com.pem' % SSL_DIR, log=logger)
-    thread = threading.Thread(target=server.serve_forever)
-    thread.start()
-    logger.info("Server is running.")
+    server.serve_forever()
 
 #    app.run(ssl_context=('%s/cert-zwift-com.pem' % SSL_DIR, '%s/key-zwift-com.pem' % SSL_DIR), port=443, threaded=True, host='0.0.0.0') # debug=True, use_reload=False)
 
